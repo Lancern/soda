9918c8139f96e287e6de83ec00359602f660bea2
Sirui Mu <msrlancern@gmail.com>
2023-12-01 01:52:35 +0800

    fix: update soname conversion logic

--- src/main.rs
+++ src/main.rs
@@ -44,9 +44,14 @@ impl Args {
 
         // If the user does not provide an output path, we form one by replacing the file name part of the input path
         // with a proper static library name.
+        //
+        // Examples of name conversion:
+        // - `/dir/libxyz.so` will be converted to `/dir/xyz.o`
+        // - `/dir/xyz.so` will be converted to `/dir/xyz.o`
 
         let mut path = self.input.clone();
-        path.set_extension("a");
+        let file_name = path.file_name().unwrap().to_str().unwrap();
+        path.set_file_name(convert_soname_to_object_name(file_name));
 
         Cow::Owned(path)
     }
@@ -76,8 +81,10 @@ fn main() -> anyhow::Result<()> {
 
     // Open the output file, preparing to write later.
     let output_path = &*args.get_output_path();
-    let mut output_file = OutputFile::create(output_path)
-        .context(format!("failed to open output file \"{}\"", output_path.display()))?;
+    let mut output_file = OutputFile::create(output_path).context(format!(
+        "failed to open output file \"{}\"",
+        output_path.display()
+    ))?;
 
     // Run the passes.
     log::info!("Running all registered passes ...");
@@ -88,12 +95,52 @@ fn main() -> anyhow::Result<()> {
     ctx.output
         .write_stream(output_file.writer())
         .map_err(|err| anyhow!(format!("{:?}", err)))
-        .context(format!("failed to write output file \"{}\"", output_path.display()))?;
+        .context(format!(
+            "failed to write output file \"{}\"",
+            output_path.display()
+        ))?;
 
     output_file.prevent_delete_on_drop();
     log::info!("Done.");
 
     Ok(())
+}
+
+/// Convert a shared library name into its corresponding object name.
+///
+/// Examples of the conversion:
+/// - `libxyz.so` will be converted to `xyz.o`
+/// - `xyz.so` will be converted to `xyz.o`
+/// - `xyz` will be converted to `xyz.o`
+///
+/// Specifically:
+/// - If the given soname does not ends with .so (regardless of case), then a plain ".o" suffix will be added to the
+///   given name and we're done.
+/// - Otherwise, replace the ".so" suffix with ".o".
+/// - If the given soname begins with "lib" (regardless of case), remove that prefix.
+fn convert_soname_to_object_name(soname: &str) -> String {
+    let name_core = 'b: {
+        if soname.len() < 3 {
+            break 'b soname;
+        }
+
+        let (file_name_wo_ext, ext_suffix) = soname.split_at(soname.len() - 3);
+        if ext_suffix.to_lowercase() != ".so" {
+            // The given soname does not ends with .so.
+            return format!("{}.o", soname);
+        }
+
+        if file_name_wo_ext.len() >= 3 {
+            let (lib_prefix, name_core) = file_name_wo_ext.split_at(3);
+            if lib_prefix.to_lowercase() == "lib" {
+                break 'b name_core;
+            }
+        }
+
+        file_name_wo_ext
+    };
+
+    format!("{}.o", name_core)
 }
 
 fn init_logger(verbosity: u8) -> Result<(), SetLoggerError> {
