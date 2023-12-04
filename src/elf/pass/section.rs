67f60aa1e49b08686f5e6cd6b37b9bbcae783d14
Sirui Mu <msrlancern@gmail.com>
2023-12-04 08:33:03 +0800

    feat: implement CopyLoadableSectionsPass

--- src/elf/pass/section.rs
+++ src/elf/pass/section.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::ops::Range;
 
 use object::elf::{PT_LOAD, SHT_PROGBITS};
 use object::read::elf::{
@@ -34,30 +35,91 @@ impl ElfPass for CopyLodableSectionsPass {
         E: ElfFileHeader,
         R: ReadRef<'d>,
     {
-        let output_sec_id = output.add_section(Vec::new(), todo!(), SectionKind::Elf(SHT_PROGBITS));
+        // TODO: make the output section's name customizable.
+        let output_sec_id = output.add_section(
+            Vec::new(),
+            "soda".as_bytes().to_vec(),
+            SectionKind::Elf(SHT_PROGBITS),
+        );
+        let output_sec = output.section_mut(output_sec_id);
+
         let mut ret = CopyLodableSectionsOutput::new(output_sec_id);
 
-        // First we copy all loadable sections to output.
-        let endian = input.endian();
-        for (seg_header, seg) in input.raw_segments().iter().zip(input.segments()) {
-            let seg_type = seg_header.p_type(endian);
-            if seg_type != PT_LOAD {
-                // Skip sections in non-loadable segments.
+        // First we collect all loadable sections.
+        let input_sections = collect_loadable_sections(input);
+        if input_sections.is_empty() {
+            return Ok(ret);
+        }
+
+        // Copy the data of the collected input sections to the output section.
+        // First calculate the size and alignment of the output section, together with the offset of each input section
+        // in the output section.
+        let mut output_sec_size = 0u64;
+        for input_sec in &input_sections {
+            let input_sec_size = input_sec.size();
+
+            output_sec_size = output_sec_size.next_multiple_of(input_sec.align());
+            ret.input_section_ranges.insert(
+                input_sec.index(),
+                output_sec_size..output_sec_size + input_sec_size,
+            );
+
+            output_sec_size += input_sec_size;
+        }
+
+        assert!(output_sec_size <= std::usize::MAX as u64);
+
+        let output_sec_align = input_sections.iter().map(|sec| sec.align()).max().unwrap();
+
+        // Then do the data copy.
+        let mut output_buffer = vec![0u8; output_sec_size as usize];
+        for input_sec in &input_sections {
+            let sec_data = input_sec.uncompressed_data()?;
+            assert_eq!(sec_data.len(), input_sec.size() as usize);
+
+            let output_range = ret.input_section_ranges.get(&input_sec.index()).unwrap();
+            let output_range = output_range.start as usize..output_range.end as usize;
+
+            let output_slice = &mut output_buffer[output_range];
+            output_slice.copy_from_slice(&sec_data);
+        }
+
+        // Set the output section's data.
+        output_sec.set_data(output_buffer, output_sec_align);
+
+        Ok(ret)
+    }
+}
+
+fn collect_loadable_sections<'d, 'f, E, R>(
+    input: &'f ElfFile<'d, E, R>,
+) -> Vec<ElfSection<'d, 'f, E, R>>
+where
+    E: ElfFileHeader,
+    R: ReadRef<'d>,
+{
+    let endian = input.endian();
+    let mut input_sections = Vec::new();
+
+    for (seg_header, seg) in input.raw_segments().iter().zip(input.segments()) {
+        let seg_type = seg_header.p_type(endian);
+        if seg_type != PT_LOAD {
+            // Skip sections in non-loadable segments.
+            continue;
+        }
+
+        // Enumerate all sections in the current segment which is loadable.
+        for input_sec in input.sections() {
+            if !is_section_in_segment(&input_sec, &seg) {
                 continue;
             }
 
-            // Enumerate all sections in the current segment which is loadable.
-            for input_sec in input.sections() {
-                if !is_section_in_segment(&input_sec, &seg) {
-                    continue;
-                }
-
-                todo!()
-            }
+            input_sections.push(input_sec);
         }
-
-        todo!()
     }
+
+    input_sections.sort_by_key(|sec| sec.address());
+    input_sections
 }
 
 #[derive(Debug)]
@@ -65,15 +127,15 @@ pub struct CopyLodableSectionsOutput {
     /// The section ID of the output section.
     pub output_section_id: SectionId,
 
-    /// Describe the relative offset of each input section within the output section.
-    pub input_section_offsets: HashMap<SectionIndex, u64>,
+    /// Describe the offset range of each input section within the output section.
+    pub input_section_ranges: HashMap<SectionIndex, Range<u64>>,
 }
 
 impl CopyLodableSectionsOutput {
     fn new(output_section_id: SectionId) -> Self {
         Self {
             output_section_id,
-            input_section_offsets: HashMap::new(),
+            input_section_ranges: HashMap::new(),
         }
     }
 }
